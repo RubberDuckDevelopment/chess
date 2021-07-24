913627ce549119338f420e18fb5c9c67033cdd9a
Mark <mark@rubberduckdeveloper.com>
2021-07-23 22:07:56 -0400

    adding FEN parsing to show a single position

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 const files = ["a", "b", "c", "d", "e", "f", "g", "h"].reverse();
-const pieces = ["pawn", "rook", "knight", "bishop", "queen", "king"];
+const pieces = [
+  {
+    name: "pawn",
+    symbol: "p",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg",
+    img_white:
+      "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg",
+    img_black:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg",
+  },
+  {
+    name: "rook",
+    symbol: "r",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg",
+    img_white:
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Chess_rlt45.svg",
+    img_black:
+      "https://upload.wikimedia.org/wikipedia/commons/f/ff/Chess_rdt45.svg",
+  },
+  {
+    name: "knight",
+    symbol: "n",
+    img: "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg",
+    img_white:
+      "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg",
+    img_black:
+      "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg",
+  },
+  {
+    name: "bishop",
+    symbol: "b",
+    img: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+    img_white:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg",
+    img_black:
+      "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg",
+  },
+  {
+    name: "queen",
+    symbol: "q",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg",
+    img_white:
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg",
+    img_black:
+      "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg",
+  },
+  {
+    name: "king",
+    symbol: "k",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg",
+    img_white:
+      "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg",
+    img_black:
+      "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg",
+  },
+];
+const startingPosition =
+  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 function returnBoardArray(boardRanks = 8, boardFiles = 8) {
   let boardArr = new Array(boardRanks * boardFiles);
@@ -38,8 +95,6 @@ function generateBoard(boardArr) {
     cell.className = `cell`;
     cell.id = `${boardArr[i].file}${boardArr[i].rank}`;
     cell.setAttribute("name", "cell");
-    //cell.textContent=`${boardArr[i].file}${boardArr[i].rank}`
-    // cell.addEventListener("mouseover",handleHover)
 
     if (boardArr[i].rankOffset === 1) {
       cell.className += " offset";
@@ -50,9 +105,20 @@ function generateBoard(boardArr) {
     if (boardArr[i].hasPosition === 1) {
       cell.className = cell.className;
       const piece = document.createElement("div");
-      piece.name = boardArr[i].piece;
+      piece.setAttribute(
+        "name",
+        `${boardArr[i].color} ${boardArr[i].piece.name}`
+      );
       piece.className = "piece";
-      piece.style.backgroundColor = boardArr[i].color;
+
+      const img = document.createElement("img");
+      if (boardArr[i].color === "white") {
+        img.src = boardArr[i].piece.img_white;
+      } else {
+        img.src = boardArr[i].piece.img_black;
+      }
+
+      piece.append(img);
       cell.append(piece);
     } else {
       //cell.className += " idk";
@@ -93,4 +159,76 @@ function formatPosition() {
   boardArr[boardIdx].piece = "pawn";
 
   resetBoard(boardArr);
+}
+
+function parseFEN() {
+  let FENentry = document.getElementById("fen-entry").value.split("/");
+
+  let boardArr = returnBoardArray().reverse();
+
+  let parsedFEN;
+  let FEN = [];
+  let cellCount = 0;
+
+  for (let i = 0; i < 8; i++) {
+    //parse each rank
+    parsedFEN = parseRank(FENentry[i]);
+    FEN.push(parsedFEN);
+  }
+  FEN.join();
+
+  //   console.log(FEN);
+
+  //console.table(FEN);
+  for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 8; i++) {
+      cellCount = j * 8 + i;
+      if (isNaN(FEN[j][i]) == false) {
+        //space
+        //console.log("space".repeat(FEN[i]));
+        for (let k = 0; k < FEN[j][i]; k++) {
+          boardArr[cellCount + k].hasPosition = 0;
+        }
+      } else if (FEN[j][i].toUpperCase() == FEN[j][i]) {
+        //white piece
+        boardArr[cellCount].hasPosition = 1;
+        boardArr[cellCount].color = "white";
+        boardArr[cellCount].piece = parsePiece(FEN[j][i]);
+      } else {
+        //black piece
+        boardArr[cellCount].hasPosition = 1;
+        boardArr[cellCount].color = "black";
+        boardArr[cellCount].piece = parsePiece(FEN[j][i]);
+      }
+    }
+  }
+
+  resetBoard(boardArr);
+}
+
+function parseRank(rank) {
+  //input string of pieces & #spaces
+  //return string with spaces separated
+  let parsedRank = [];
+  for (let i = 0; i < 8; i++) {
+    if (isNaN(rank[i]) === true) {
+      //piece
+      parsedRank.push(rank[i]);
+    } else {
+      //space
+      for (let j = 0; j < rank[i]; j++) {
+        parsedRank.push(" ");
+      }
+    }
+  }
+  return parsedRank;
+}
+
+function parsePiece(pieceAbbreviation) {
+  //input piece abbrevation
+  //return piece object
+  let parsedPiece = pieces.filter(
+    (piece) => piece.symbol == pieceAbbreviation.toLowerCase()
+  )[0];
+  return parsedPiece;
 }
